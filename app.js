30a51ddacb1cedcc92b5fbdcc3cdffb36e378094
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-05 12:18:33 +0200

    Add Search

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 const auth = "563492ad6f9170000100000145a75551fd4b4ade91fe329c4fdb29e4";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
 
 //-------Event Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchPhotos(searchValue);
+});
 
 //-------------Functions
+
+function updateInput(event) {
+  searchValue = event.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
     "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+      <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
     {
       method: "GET",
       headers: {
